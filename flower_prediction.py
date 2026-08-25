c3ea3f9c01d81941bf7b091b4c096e64ca11ea5a
Usman Asif <usman.hussain@10pearls.com>
2026-08-25 14:03:58 +0500

    Setup hello world model (iris)

---
+++ flower_prediction.py
@@ -0,0 +1,19 @@
+import os
+import joblib
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+X, y = load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = LogisticRegression(max_iter=200)
+
+model.fit(X_train, y_train)
+
+artifact_dir = "artifacts"
+os.makedirs(artifact_dir, exist_ok=True)
+
+model_path = os.path.join(artifact_dir, "model.pkl")
+joblib.dump(model, model_path)
